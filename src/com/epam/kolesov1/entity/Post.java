bc9826d103a200a9ab8001968ebe1113dd2a1d76
Artem Kolesov <kolesovartem@gmail.com>
2016-04-04 22:23:36 +0300

    update post class and add db pool in web.xml

--- src/com/epam/kolesov1/entity/Post.java
+++ src/com/epam/kolesov1/entity/Post.java
@@ -1,5 +1,55 @@
 package com.epam.kolesov1.entity;
 
+import java.sql.Timestamp;
+
 public class Post {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getPostDate() {
+		return postDate;
+	}
+
+	public void setPostDate(Timestamp postDate) {
+		this.postDate = postDate;
+	}
+
+	private String title;
+	private String content;
+	private Timestamp postDate;
+
+	public Post() {
+
+	}
+
+	public Post(int id, String title, String content, Timestamp postDate) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.postDate = postDate;
+	}
 
 }
